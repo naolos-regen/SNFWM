ecae46aeb917af26b251ec50111dc24131b5c8c6
naolos <naolos.regen@proton.me>
2025-10-03 12:10:59 +0200

    gitignore

---
+++ runner.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+DISPLAY_NUM=:2
+XEPHYR_RES="1600x900"
+
+killall Xephyr 2>/dev/null
+
+Xephyr $DISPLAY_NUM -ac -screen $XEPHYR_RES -br &
+XEPHYR_PID=$!
+
+sleep 1
+
+export DISPLAY=$DISPLAY_NUM
+
+./scfwm &
+WM_PID=$!
+
+sleep 1
+
+alacritty &
+
+read -p "press to exit"
+
+kill $WM_PID
+kill $XEPHYR_PID
