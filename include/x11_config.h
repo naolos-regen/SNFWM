0c5b31db0e09add4afa50df521e8f4bf0ae14927
naolos <naolos.regen@proton.me>
2025-10-07 22:35:29 +0200

    basic config

---
+++ include/x11_config.h
@@ -0,0 +1,10 @@
+#ifndef SCFWM_X11_CONFIG_H
+#define SCFWM_X11_CONFIG_H
+
+#include <X11/keysym.h>
+
+#define MODIFIER_PREFIX 133
+#define TERM_PROG       "alacritty"
+#define KEY_ALACRITTY   36
+
+#endif
