dcde961018bccffcaf43ce7800d3884633ae2b0d
naolos <naolos.regen@proton.me>
2025-10-08 21:34:31 +0200

    x11_helpers what? and ofc an idea poping into my head

--- include/x11_helpers.h
+++ include/x11_helpers.h
@@ -12,13 +12,13 @@
 
 struct x11_dp
 {
-        Display *display;
-        Window root;
+    Display *display;
+    Window root;
 };
 
 extern x11_display display;
 extern x11_display x11_display_instance();
-extern int x11_init_display (x11_display *dp);
+extern int x11_init_display(x11_display *dp);
 extern int x11_free (x11_display *dp);
 extern int x11_error_handler (Display *display, const XErrorEvent *event);
 extern int x11_set_masks (const x11_display* screen);
--- src/x11_events/events/destroy_notify.c
+++ src/x11_events/events/destroy_notify.c
@@ -1,6 +1,10 @@
 #include "../../../include/x11_events.h"
 #include "../../../include/logger.h"
 
+/* TODO: What needs to be implemented is sort of a way to store all windows/screens 
+ *       and find corresponding window that we try to remove using XDestroyNotifyEvent
+ *       so first of all i need to create a list innit 
+ */
 void destroy_notify(const XEvent *event)
 {
     log_info("to implement");
