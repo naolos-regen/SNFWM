37aa81b0a5982d681d59acdcf92c79e415654ab7
naolos <naolos.regen@proton.me>
2025-10-14 16:56:56 +0200

    just a thought

--- src/libx/x_exit.c
+++ src/libx/x_exit.c
@@ -1,7 +1,17 @@
 #include "../../include/libx.h"
+#include "../../include/logger.h"
 
 void
 x_exit (int errno)
 {
+        /* TODO: I have thought of getting stack-based-error or 
+         *       who called the function smoothen up the 
+         *       finding processes.
+         *       maybe it's possible (but it will cost extra heap)
+         */
+        switch (errno)
+        {
+                case OUT_OF_MEMORY: log_fatal("Out of memory error"); break;
+        }
         exit(errno);
 }
