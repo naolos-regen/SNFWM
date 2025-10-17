005a2b46fccec68cc1e4f00682cd6058ce908c4c
naolos <naolos.regen@proton.me>
2025-10-17 11:25:12 +0200

    logging applied on main_loop and new_window logging bugs

--- src/x11_events/jobs/x11_new_window.c
+++ src/x11_events/jobs/x11_new_window.c
@@ -1,22 +1,46 @@
 #include "../../../include/linkedlist.h"
 #include "../../../include/x11_helpers.h"
 #include "../../../include/x11_data.h"
+#include "../../../include/x11_jobs.h"
+#include "../../../include/logger.h"
 
-void 
+void
 new_window (const XCreateWindowEvent *e)
 {
-        t_window_list *win;
-        snfwm_screen *scr;
-
         if (e->override_redirect)
                 return;
-        
-        win = list_find_window(dpy->head, e->window);
-        scr = find_screen(e->parent);
 
-        if (scr && !win && e->window != scr->key_window && e->window != scr->bar_window)
+        snfwm_screen *scr = find_screen(e->parent);
+        if (!scr)
         {
-                win = add_to_list(scr, win->window->window);
-                win->window->state = STATE_UNMAPPED;
+                log_error("No screen found for parent window %lu", e->parent);
+                return;
+        }
+
+        t_window_list *existing_win = list_find_window(dpy->head, e->window);
+        if (existing_win)
+        {
+                log_debug("Window %lu already in list", e->window);
+                return;
+        }
+
+        if (e->window == scr->key_window || e->window == scr->bar_window)
+        {
+                log_debug("Skipping special window %lu", e->window);
+                return;
+        }
+
+        t_window_list *new_win = add_to_list(scr, e->window);
+        if (new_win)
+        {
+                log_info("Added new window %lu to list", e->window);
+                if (new_win->window)
+                {
+                        new_win->window->state = STATE_UNMAPPED;
+                }
+        }
+        else
+        {
+                log_error("Failed to add window %lu to list", e->window);
         }
 }
--- src/x11_helpers/x11_main_loop.c
+++ src/x11_helpers/x11_main_loop.c
@@ -1,5 +1,14 @@
 #include "../../include/x11_helpers.h"
 #include "../../include/x11_events.h"
+#include "../../include/logger.h"
+
+static char *event_name[] = {
+        "", "", "KeyPress", "KeyRelease", "ButtonPress", "ButtonRelease",
+        "MotionNotify", "EnterNotify", "LeaveNotify", "FocusIn", "FocusOut",
+        "KeymapNotify", "Expose", "GraphicsExpose", "NoExpose", "VisibilityNotify",
+        "CreateNotify", "DestroyNotify", "UnmapNotify", "MapNotify", "MapRequest",
+        "ReparentNotify", "ConfigureNotify", "ConfigureRequest", "GravityNotify"
+};
 
 void 
 x11_main_loop (void)
@@ -8,7 +17,10 @@ x11_main_loop (void)
         while(1)
         {
                 XNextEvent(dpy->display, &ev);
-                if (handler[ev.type]) 
+
+                if (handler[ev.type]){
+                        log_debug("Event type: %s (%d)", event_name[ev.type], ev.type);
                         handler[ev.type](&ev);
+                }
         }
 }
